53424eaf035ef2dbe36ffc131ff18eb336d9df89
Prnay Jain <prnayjain@gmail.com>
2020-08-04 23:02:05 +0530

    delete old tasks on reinstall

--- install.bat
+++ install.bat
@@ -2,9 +2,12 @@
 FOR /F "delims=" %%i IN ('whoami /user /fo csv /nh') DO set info=%%i
 cd %~dp0
 PrepareTaskXml.exe %info%
-schtasks /Create /XML RecordLogIn.xml /tn RecordLogIn
+schtasks /delete /tn RecordLogIn /f
+schtasks /create /XML RecordLogIn.xml /tn RecordLogIn
 del RecordLogIn.xml
-schtasks /Create /XML RecordLogOut.xml /tn RecordLogOut
+schtasks /delete /tn RecordLogOut /f
+schtasks /create /XML RecordLogOut.xml /tn RecordLogOut
 del RecordLogOut.xml
-schtasks /Create /XML Update.xml /tn Update
+schtasks /delete /tn Update /f
+schtasks /create /XML Update.xml /tn Update
 del Update.xml
